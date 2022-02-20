1ba852fa1ea61d20771274acc61e214905bbb50a
MINSEOK PARK <89582736+seiren1005@users.noreply.github.com>
2022-02-21 08:41:35 +0900

    [DELETE] 완료되어 삭제

--- github_test.java
+++
@@ -1,9 +0,0 @@
-public class github_test {
-    public static void main(String[] args) {
-        
-        System.out.println("github를 테스트 중입니다.");
-        
-        System.out.println("내용을 추가하였습니다.");
-    }
-}
-
